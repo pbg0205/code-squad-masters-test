812413a81f65c18cca2a4390faf5ff7c50fb8bfe
pbg0205 <pbg0205@naver.com>
2020-12-08 11:04:17 +0900

    feat : add InputView class for input command

---
+++ step-2/src/InputView.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputView {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static String inputCommand() {
+        System.out.print("CUBE> ");
+        return SCANNER.nextLine();
+    }
+}
