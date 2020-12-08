cb52bfad8eb705b02454c031b5290973aa1d06ba
pbg0205 <pbg0205@naver.com>
2020-12-08 17:43:21 +0900

    feat : add application logic for execute

--- step-2/src/Application.java
+++ step-2/src/Application.java
@@ -1,5 +1,18 @@
+import java.util.List;
+
 class Application extends Thread {
+    private Cube cube;
+
+    public Application() {
+        this.cube = new Cube();
+    }
+
     @Override
     public void run() {
+        List<Command> commandList;
+        while(true) {
+            commandList = CommandsSeperator.makeCommands(InputView.inputValues());
+            cube.move(commandList);
+        }
     }
 }
