f97b63b188ec60c4430a1e278bd0b3420c8a5fb2
pbg0205 <pbg0205@naver.com>
2020-12-09 23:21:25 +0900

    feat : Add application for execute

--- step-3/src/Application.java
+++ step-3/src/Application.java
@@ -1,6 +1,20 @@
+import java.util.List;
+
 class Application extends Thread {
+    private Cube cube;
+
+    Application() {
+        this.cube = new Cube();
+    }
+
     @Override
     public void run() {
+        List<Command> commandList;
 
+        while(true) {
+            cube.printStatus();
+            commandList = ValueSeperator.getCommandList(InputView.inputValues());
+            cube.rotate(commandList);
+        }
     }
 }
